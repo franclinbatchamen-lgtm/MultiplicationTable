2b522d48d82248fd9eabcd49ee4e4603c9a45c34
franclinbatchamen-lgtm <franclinbatchamen@gmail.com>
2026-04-02 20:20:07 +0200

    added explicit intent

---
+++ MultiplyTable/app/src/main/java/com/example/multiplytable/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.multiplytable
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val MultiplyButton = findViewById<Button>(R.id.MultiplyButton)
+        MultiplyButton.setOnClickListener {
+            // create the explicit intent
+            val intent = Intent(this, MultiplicationTable::class.java)
+            // start the activity
+            startActivity(intent)
+        }
+
+
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
