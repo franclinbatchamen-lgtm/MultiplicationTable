5a28abbb7c1bed5803d49ca6b11a22f564cd837f
franclinbatchamen-lgtm <franclinbatchamen@gmail.com>
2026-04-02 21:03:20 +0200

    linked the two classes

--- MultiplyTable/app/src/main/java/com/example/multiplytable/MainActivity.kt
+++ MultiplyTable/app/src/main/java/com/example/multiplytable/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.multiplytable
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,11 +12,21 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
+
         val MultiplyButton = findViewById<Button>(R.id.MultiplyButton)
+        val editTextNumber = findViewById<EditText>(R.id.editTextNumber)
         MultiplyButton.setOnClickListener {
+
             // create the explicit intent
             val intent = Intent(this, MultiplicationTable::class.java)
+
+            // add the table number to the intent
+            intent.putExtra("tableNumber", editTextNumber.text.toString())
+
             // start the activity
             startActivity(intent)
         }
--- MultiplyTable/app/src/main/java/com/example/multiplytable/MultiplicationTable.kt
+++ MultiplyTable/app/src/main/java/com/example/multiplytable/MultiplicationTable.kt
@@ -1,6 +1,7 @@
 package com.example.multiplytable
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,31 @@ class MultiplicationTable : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_multiplication_table)
+
+        // get the table number from the intent
+        val bundle: Bundle? = intent.extras
+        val tableString: String? = bundle?.getString("tableNumber")
+
+        // convert the table number to an integer
+        val tableNumber = tableString?.toInt()
+
+        // display the heading so we can see the value in action
+        val multiplicationTable = findViewById<TextView>(R.id.multiTableView)
+        multiplicationTable.text = "$tableNumber x table"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
