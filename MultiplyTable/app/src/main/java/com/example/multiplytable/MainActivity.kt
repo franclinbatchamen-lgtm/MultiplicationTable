41d3c8e02d54b909489e38516fc372465a20c4e3
franclinbatchamen-lgtm <franclinbatchamen@gmail.com>
2026-04-03 14:46:47 +0200

    added animation going to the next page

--- MultiplyTable/app/src/main/java/com/example/multiplytable/MainActivity.kt
+++ MultiplyTable/app/src/main/java/com/example/multiplytable/MainActivity.kt
@@ -18,6 +18,15 @@ class MainActivity : AppCompatActivity() {
         val MultiplyButton = findViewById<Button>(R.id.MultiplyButton)
         val editTextNumber = findViewById<EditText>(R.id.editTextNumber)
         MultiplyButton.setOnClickListener {
+            MultiplyButton.setOnClickListener {
+                it.animate().scaleX(0.9f).scaleY(0.9f).setDuration(100).withEndAction {
+                    it.animate().scaleX(1f).scaleY(1f).duration = 100
+                }
+
+                val intent = Intent(this, MultiplicationTable::class.java)
+                intent.putExtra("tableNumber", editTextNumber.text.toString())
+                startActivity(intent)
+            }
 
             // create the explicit intent
             val intent = Intent(this, MultiplicationTable::class.java)
@@ -27,6 +36,7 @@ class MainActivity : AppCompatActivity() {
 
             // start the activity
             startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
